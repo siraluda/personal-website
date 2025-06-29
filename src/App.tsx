353f9b7b82807928819a56bbf6eef622b60bdd4d
Abdul-Waris <siraluda.dev@gmail.com>
2025-06-28 22:26:47 -0700

    Edit App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,13 +16,13 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Abdul-Waris Personal Website</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Coming soon...
         </p>
       </div>
       <p className="read-the-docs">
